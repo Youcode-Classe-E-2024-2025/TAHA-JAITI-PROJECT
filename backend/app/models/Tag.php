5a61a51c8d4800a86226eb823aea245360fa0090
Taha <kyojin.rar@gmail.com>
2025-01-02 09:57:35 +0100

    FEAT: Add Tag model and routes; implement tag creation and assignment to tasks

---
+++ backend/app/models/Tag.php
@@ -0,0 +1,76 @@
+<?php
+
+class Tag{
+
+    private PDO $db;
+    private $id;
+    private $taskId;
+    private $name;
+    private $color;
+
+    public function __construct()
+    {
+        $this->db = Database::getConnection();
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setColor($color){
+        $this->color = $color;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setTask($id)
+    {
+        $this->taskId = $id;
+    }
+
+    public function createTag(): bool | int{
+        $sql = "INSERT INTO tags(name, color) VALUES (:name, :color)";
+        
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':color', $this->color);
+
+        if ($stmt->execute()){
+            $this->setId((int) $this->db->lastInsertId());
+            return $this->id;
+        } else {
+            return false;
+        }
+    }
+
+    public function getTags(): array | null {
+        $sql = "SELECT * FROM tags";
+
+        $stmt = $this->db->prepare($sql);
+        
+        if ($stmt->execute()){
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        
+        return null;
+    }
+
+    public function assignTag(): bool {
+        $sql = "INSERT INTO task_tags (task_id, tag_id)
+                VALUES (:task_id, :tag_id)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(':task_id', $this->taskId);        
+        $stmt->bindParam(':tag_id', $this->id);
+        
+        if ($stmt->execute()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
--- backend/public/index.php
+++ backend/public/index.php
@@ -10,22 +10,29 @@ Database::initializeDatabase();
 
 $router = new Router();
 
+//auth routes
 $router->addRoute('POST', '/register', 'AuthController@register');
 $router->addRoute('POST', '/login', 'AuthController@login');
 $router->addRoute('GET', '/logout', 'AuthController@logout');
 
+//project routes
 $router->addRoute('POST', '/project', 'ProjectController@createProject');
 $router->addRoute('POST', '/project/assign', 'ProjectController@assignMember');
 $router->addRoute('GET', '/project', 'ProjectController@getAllProjects');
 $router->addRoute('DELETE', '/project', 'ProjectController@deleteProject');
 
+//task routes
 $router->addRoute('POST', '/task', 'TaskController@createTask');
 $router->addRoute('POST', '/task/assign', 'TaskController@assignTask');
 $router->addRoute('GET', '/task', 'TaskController@getAllTasks'); //get project tasks based on project id
 $router->addRoute('DELETE', '/task', 'TaskController@deleteTask');
 
+//category routes
 $router->addRoute('POST', '/category', 'CategoryController@createCat');
 $router->addRoute('GET', '/category', 'CategoryController@getAllCategories');
 $router->addRoute('POST', '/category/assign', 'CategoryController@assignCat');
+
+//tag routes
+$router->addRoute('POST', '/tag', 'TagController@createTag');
 
 $router->run($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
